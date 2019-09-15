8b4f4f095679a1787e3358fead2b66188956c097
gyengus <gyengus@gmail.com>
2019-09-15 20:02:07 +0200

    Add Swagger integration tests

---
+++ service/src/test/java/hu/gyengus/thermometerservice/SwaggerIT.java
@@ -0,0 +1,40 @@
+package hu.gyengus.thermometerservice;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@SpringBootTest(classes = { ThermometerServiceApplication.class })
+@AutoConfigureMockMvc
+public class SwaggerIT {
+    private static final int HTTP_STATUS_OK = 200;
+    private static final String SWAGGER_JSON_URL = "/v2/api-docs";
+    private static final String SWAGGER_UI_URL = "/swagger-ui.html";
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void testSwaggerJsonEndpointShouldReturnOK() throws Exception {
+        // GIVEN
+        // WHEN
+        MvcResult result = mvc.perform(get(SWAGGER_JSON_URL)).andReturn();
+        // THEN
+        assertEquals(HTTP_STATUS_OK, result.getResponse().getStatus());
+    }
+
+    @Test
+    void testSwaggerUIEndpointShouldReturnOK() throws Exception {
+        // GIVEN
+        // WHEN
+        MvcResult result = mvc.perform(get(SWAGGER_UI_URL)).andReturn();
+        // THEN
+        assertEquals(HTTP_STATUS_OK, result.getResponse().getStatus());
+    }
+}
